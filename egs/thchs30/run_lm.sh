fa994629c10652dad12e35f7de2d3c9236ec89e8
du-ud <qiangasr@gmail.com>
2022-07-08 19:53:05 +0800

    update run_lm.sh

--- egs/thchs30/run_lm.sh
+++ egs/thchs30/run_lm.sh
@@ -1,10 +1,13 @@
 dir=exp/lm
 lm_dir=
-mdl_dir=exp/chain-skip/tdnn-f-cn
+gmm_mdl_dir=exp/tri4b
+nn_mdl_dir=exp/chain-skip/tdnn-f-cn
 
 mkdir $dir
 tar czvf $dir/corpus.lm_e-7.tar.gz  data_thchs30/lm_word/word.3gram.lm
 
 ./utils/format_lm.sh  data/lang $dir/corpus.lm_e-7.tar.gz  data/lang $dir/lang_test
 
-./utils/mkgraph.sh  --self-loop-scale 1.0 $dir/lang_test $mdl_dir  $mdl_dir/graph
+./utils/mkgraph.sh   $dir/lang_test $gmm_mdl_dir  $gmm_mdl_dir/graph
+
+./utils/mkgraph.sh  --self-loop-scale 1.0 $dir/lang_test $nn_mdl_dir  $nn_mdl_dir/graph
